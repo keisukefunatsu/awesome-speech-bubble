991759759847b20f441bcd6c2adc32ed48923e96
funatsu <keisuekyan2012@gmail.com>
2017-06-26 18:00:47 +0900

    Nothing to say... it works!

--- awesome-speech-bubble.php
+++ awesome-speech-bubble.php
@@ -22,134 +22,148 @@ register_activation_hook( __FILE__, array( 'AwesomeSpeechBubble', 'install' ) );
 $awesome_speech_bubble = new AwesomeSpeechBubble();
 $awesome_speech_bubble->register();
 
-class AwesomeSpeechBubble
-{
+class AwesomeSpeechBubble {
 
-	static function install() {
-		// add_option( 'awesome_speech_bubble_data', '' );
-	}
+    static function install() {
+    }
 
-	public function register(){
-		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
-		// add_shortcode('awesome_speech_bubble', array( $this, 'handle_shortcode' ) );
-	}
+    public function register() {
+        add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
+    }
 
-	/**
- 	* Fires on plugins_loaded hook.
- 	*
- 	* @param  none
- 	* @return none
- 	*/
-	public function plugins_loaded() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'asb_wp_enqueue_scripts' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'asb_admin_enqueue_scripts' ) );
-		add_action( 'edit_form_after_title', array( $this, 'display_app' ));
-		add_action( 'wp_ajax_awesome_speech_bubble', array( $this, 'wp_ajax_awesome_speech_bubble' ));
-		add_action( 'wp_ajax_nopriv_awesome_speech_bubble', array( $this, 'wp_ajax_awesome_speech_bubble' ));
-	}
+    /**
+     * Fires on plugins_loaded hook.
+     *
+     * @param  none
+     * @return none
+     */
+    public function plugins_loaded() {
+        add_action( 'wp_enqueue_scripts', array( $this, 'asb_wp_enqueue_scripts' ) );
+        add_action( 'admin_enqueue_scripts', array( $this, 'asb_admin_enqueue_scripts' ) );
+        add_action( 'edit_form_after_title', array( $this, 'display_app' ) );
+        add_action( 'wp_ajax_awesome_speech_bubble', array( $this, 'wp_ajax_awesome_speech_bubble' ) );
+        add_action( 'wp_ajax_nopriv_awesome_speech_bubble', array( $this, 'wp_ajax_awesome_speech_bubble' ) );
+        add_shortcode( 'asp_sc', array( $this, 'asp_shortcode' ) );
+    }
 
-	/**
-	* Enqueue user script for post page
-	*
-	* @param  none
-	* @return none
-	*/
-	public function asb_wp_enqueue_scripts() {
-		if ( is_singular() ) {
-			wp_enqueue_script(
-				'awesome-speech-bubble-script',
-				plugins_url( 'js/awesome-speech-bubble.js', __FILE__ ),
-				array( 'jquery' ),
-				filemtime(dirname(__FILE__) . '/js/awesome-speech-bubble.js')
-			);
-		}
-	}
+    /**
+     * Enqueue user script for post page
+     *
+     * @param  none
+     * @return none
+     */
+    public function asb_wp_enqueue_scripts() {
+        if ( is_singular() ) {
+            wp_enqueue_script(
+                'awesome-speech-bubble-script',
+                plugins_url( 'js/awesome-speech-bubble.js', __FILE__ ),
+                array( 'jquery' ),
+                filemtime( dirname( __FILE__ ) . '/js/awesome-speech-bubble.js' )
+            );
+        }
+        wp_enqueue_style(
+            'awesome-speech-bubble-style',
+            plugins_url( 'css/asp_dialog.css', __FILE__ ),
+            array(),
+            filemtime( dirname( __FILE__ ) . '/css/asp_dialog.css' )
+        );
+    }
 
-	/**
-	* Enqueue admin script for edit page
-	*
-	* @param  none
-	* @return none
-	*/
-	public function display_app() {
-		echo '<div id="app"></div>';
-	}
-	public function asb_admin_enqueue_scripts() {
-			wp_enqueue_script(
-				'admin-awesome-speech-bubble-script',
-				plugins_url( 'js/admin-awesome-speech-bubble.js', __FILE__ ),
-				array( 'jquery' ),
-				filemtime(dirname(__FILE__) . '/js/admin-awesome-speech-bubble.js')
-			);
-			wp_enqueue_media();
-			wp_enqueue_script(
-				'awesome-speech-bubble-vue',
-				plugins_url( 'js/build.js', __FILE__ ),
-				'',
-				filemtime(dirname(__FILE__) . '/js/build.js')
-			);
+    /**
+     * Enqueue admin script for edit page
+     *
+     * @param  none
+     * @return none
+     */
+    public function display_app() {
+        echo '<div id="app"></div>';
+    }
 
-			$ajax_url = admin_url('admin-ajax.php');
-			$awesome_nonce = wp_create_nonce('awesome_speech_bubble');
-			$awesome_args = array(
-				'action' => 'awesome_speech_bubble',
-				'nonce' => $awesome_nonce,
-				);
-			?>
-			<script type="text/javascript">
-				var awesome_speech_bubble_uri = '<?php echo esc_url($ajax_url); ?>';
-				var awesome_speech_bubble_args = <?php echo json_encode($awesome_args); ?>;
-			</script>
-			<?php
+    public function asb_admin_enqueue_scripts() {
+        wp_enqueue_script(
+            'admin-awesome-speech-bubble-script',
+            plugins_url( 'js/admin-awesome-speech-bubble.js', __FILE__ ),
+            array( 'jquery' ),
+            filemtime( dirname( __FILE__ ) . '/js/admin-awesome-speech-bubble.js' )
+        );
+        wp_enqueue_media();
+        wp_enqueue_script(
+            'awesome-speech-bubble-vue',
+            plugins_url( 'js/build.js', __FILE__ ),
+            '',
+            filemtime( dirname( __FILE__ ) . '/js/build.js' )
+        );
 
-	}
+        $ajax_url = admin_url( 'admin-ajax.php' );
+        $awesome_nonce = wp_create_nonce( 'awesome_speech_bubble' );
+        $awesome_args = array(
+            'action' => 'awesome_speech_bubble',
+            'nonce' => $awesome_nonce,
+        );
+        ?>
+        <script type="text/javascript">
+            var awesome_speech_bubble_uri = '<?php echo esc_url( $ajax_url ); ?>';
+            var awesome_speech_bubble_args = <?php echo json_encode( $awesome_args ); ?>;
+        </script>
+        <?php
 
-	/**
-	* Ajax function to update
-	*
-	* @param  none
-	* @return none
-	*/
-	public function wp_ajax_awesome_speech_bubble() {
-		nocache_headers();
-		// Do nothing when nonce does not match.
-		if (! isset( $_GET['nonce'] ) || ! wp_verify_nonce( $_GET['nonce'], 'awesome_speech_bubble' ) ) {
-			return;
-		}
+    }
 
-		if ( $_GET['init'] == true ) {
-			echo json_encode( get_option( 'awesome_speech_bubble_data' ) );
-			die();
-		}
+    /**
+     * Ajax function to update
+     *
+     * @param  none
+     * @return none
+     */
+    public function wp_ajax_awesome_speech_bubble() {
+        nocache_headers();
+        // Do nothing when nonce does not match.
+        $input_get = filter_input_array( INPUT_GET, FILTER_SANITIZE_STRING );
 
-		if ( ! empty( $_GET['image_url'] ) && ! empty( $_GET['style_type'] ) && ! empty( $_GET['person_name'] ) ) {
-			$image_url = esc_url( $_GET['image_url'] );
-			$style_type = sanitize_text_field( $_GET['style_type'] );
-			$person_name = sanitize_text_field( $_GET['person_name'] );
-			$id = mt_rand();
+        if ( ! isset( $input_get[ 'nonce' ] ) || ! wp_verify_nonce( $input_get[ 'nonce' ], 'awesome_speech_bubble' ) ) {
+            return;
+        }
 
-			$new_args = array(
-				$id => array(
-					'image_url' => $image_url,
-					'style_type' => $style_type,
-					'person_name' => $person_name,
-				)
-			);
-			if ( ! empty( get_option( 'awesome_speech_bubble_data' ) ) ) {
-				$present_args = get_option( 'awesome_speech_bubble_data' );
-				$merged_args = array_merge( $new_args, $present_args );
-				update_option( 'awesome_speech_bubble_data', $merged_args ) ;
-			}
-			else {
-				update_option( 'awesome_speech_bubble_data', $new_args );
-			}
-			// delete_option('awesome_speech_bubble_data');
-			echo json_encode( get_option( 'awesome_speech_bubble_data' ) );
+        if ( input_get[ 'init' ] == true ) {
+            $users = get_users();
+            $user_info = array();
+            foreach ( $users as $user ) {
+                $name = array( 'name' => get_the_author_meta( 'display_name', $user->ID ) );
+                $user_id = array( 'id' => $user->ID );
+                array_push( $user_info, array_merge( $name, $user_id ) );
+            }
+            echo json_encode( $user_info );
+            die();
+        }
+        die();
+    }
 
-		}
-		else{
-			print_r( 'Please fill in all params' );
-		}
-		die();
-	}
+    /**
+     * return Speech Bubble HTML
+     *
+     * @return string HTML
+     */
+    public function asp_shortcode( array $attr ) {
+        $user_id = $attr[ 'id' ];
+        $message = $attr[ 'message' ];
+        $position = $attr[ 'pos' ];
+        $size = $attr[ 'size' ];
+        $avatar = get_avatar( $user_id, $size );
+        ?>
+        <div class="asp_dialog_style">
+            <div class="asp_dialog_<?php echo $position; ?>">
+                <div class="asp_avatar">
+                    <?php echo $avatar; ?>
+                </div>
+                <div class="asp_message_<?php echo $position; ?>">
+                    <p><?php echo $message; ?></p>
+                </div>
+            </div>
+        </div>
+
+
+        <?php
+    }
+
+
 }
--- js/components/App.vue
+++ js/components/App.vue
@@ -1,22 +1,26 @@
 <template>
-    <div>
-      <div class="awesome_speech_bubble_list" v-for="item in box_list">
-        <img :src="item.image_url" class="uploaded-thumbnail">
-      </div>
-      <input v-model="box_params.person_name" type="" name="" value="名前を入力">
-      <select v-model="box_params.style_type" class="" name="">
-        <option>LINE</option>
-        <option>modern</option>
-      </select>
-
-      <button @click.prevent="clearInput">Show</button>
-      <button @click.prevent="createProfile">Create</button>
-      <button @click.prevent="openUploader">Choose photo</button>
-      <span id="media"></span>
-    </div>
-
-
-
+  <div>
+    <select v-model=selectedUser>
+      <option disabled value="0">
+        Select User
+      </option>
+      <option v-for="(value, key) in asp_params" :value="value.id">
+        {{value.name}}
+      </option>
+    </select>
+    <select v-model=selectedPosition>
+      <option v-for="value in positions" :value="value.position">
+        {{value.position}}
+      </option>
+    </select>
+    <select v-model=selectedSize>
+      <option v-for="size in sizes" :value="size.value">
+        {{size.type}}
+      </option>
+    </select>
+    <textarea v-model="message" placeholder="enter text"></textarea>
+    <button type="button" name="button" @click="insertShortCode">insert</button>
+  </div>
 </template>
 
 <script>
@@ -24,79 +28,38 @@
     import Modal from './Modal.vue';
 
     export default {
-      components: { 'asp-modal' : Modal },
       data () {
         return {
-           message: 'test',
-           box_params: {
-              image_url: '',
-              style_type: 'LINE',
-              person_name: 'default',
-           },
-           box_list : {},
+            message: '',
+            selectedUser : '',
+            selectedPosition : 'left',
+            selectedSize: 'small',
+            positions : [
+                { position: 'left' },
+                { position: 'right' }
+            ],
+            sizes: [
+                { type: 'small', value: 32 },
+                { type: 'medium', value: 50 },
+                { type: 'large', value: 100 }
+            ],
+            asp_params : {},
         }
       },
       computed: {
 
       },
       methods : {
-        createProfile :function() {
-          var params = Object.assign( this.box_params , awesome_speech_bubble_args)
-          this.$http.get(awesome_speech_bubble_uri + '?action=awesome_speech_bubble', { params: params } ).then(response => {
-            // get body data
-            this.box_list = response.body;
-            console.log(response.body)
-            console.log(this.box_list)
-
-          }, response => {
-            // error callback
-          });
+        insertShortCode : function() {
+            send_to_editor( '[asp_sc id="' + this.selectedUser + '" message="' + this.message +'" pos="' + this.selectedPosition + '" size="' + this.selectedSize + '"]' )
         },
-        displayImageThumbNail: function() {
-
+        showEditWindow : function() {
         },
-
-        clearInput: function() {
-          this.box_params = {
-            image_url: '',
-            style_type: '',
-            person_name: '',
-          }
-        },
-
-        openUploader :function() {
-          var custom_uploader
-          var that = this
-            if (custom_uploader) {
-                custom_uploader.open();
-                return;
-            }
-            custom_uploader = wp.media({
-                title: 'Choose Image',
-                library: {
-                    type: 'image'
-                },
-                button: {
-                    text: 'Choose Image'
-                },
-                multiple: false
-            });
-
-            custom_uploader.on('select', function() {
-                var file = custom_uploader.state().get('selection').first().toJSON();
-                (function($){
-                  $('.uploaded-thumbnail').remove();
-                  $('#media').append('<img class="uploaded-thumbnail" src="'+file.url+'" />');
-               })(jQuery)
-              that.box_params.image_url = file.url
-            });
-            custom_uploader.open();
-        }
       },
       created: function() {
         var params = awesome_speech_bubble_args
         this.$http.get(awesome_speech_bubble_uri + '?action=awesome_speech_bubble&init=true', { params: params } ).then(response => {
-          this.box_list = response.body;
+          this.asp_params = response.body;
         }, response => {
           // error callback
         });
